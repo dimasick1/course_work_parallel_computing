167024b3f0628062c41cec8f8ce1a5ff5a680505
dimasick1 <71151028+dimasick1@users.noreply.github.com>
2021-05-21 08:24:09 +0300

    feat:added basic functionality
    Developed basic functions, class Indexer, yet w.o. implementation.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
- 
+ import time
+
+from indexer import Indexer
+from utility import *
+
+
+def main() -> None:
+    dir_path = '/home/demon/Desktop/course_work_parallel_computing/datasets/aclImdb'
+    path = Path(dir_path)
+
+    pattern = generate_path_pattern()
+    paths = generate_list_of_paths(path, pattern)
+
+    num_of_files = len(paths)
+
+    indexer = Indexer()
+
+    results = []
+    index_dicts = []
+    draw_results(results, num_of_files)
+    print('Sameness check: ', sameness_dict_check(index_dicts))
+
+    write_index_to_json(index_dicts[0])
+
+
+if __name__ == '__main__':
+    main()
+
